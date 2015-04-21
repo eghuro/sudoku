75859e66cac02ebef28f1be62cf4b987cbec3005
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-04-21 18:10:01 +0200

    Storer

--- src/cz/cuni/mff/mansuroa/sudoku/Storer.java
+++ src/cz/cuni/mff/mansuroa/sudoku/Storer.java
@@ -6,6 +6,19 @@
 package cz.cuni.mff.mansuroa.sudoku;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -13,6 +26,52 @@ import java.io.File;
  */
 public class Storer {
     public static void store(Sudoku sudoku, File file) throws StoreException {
+        try {
+            Document doc = createDocument(sudoku);
+            storeDocument(doc, file);
+            
+        } catch (ParserConfigurationException | TransformerException ex) {
+            throw new StoreException(ex);
+        }
+    }
+
+    private static void createElements(Sudoku sudoku, Document doc, Element rootElement) {
+        int size = sudoku.getSize();
+        for (int i = 0; i < size; i++ ) {
+            for (int j = 0; j < size; j++ ) {
+                Element entry = createEntry(sudoku, i, j, doc);
+                rootElement.appendChild(entry);
+            }
+        }
+    }
+
+    private static Element createEntry(Sudoku sudoku, int row, int col, Document doc) {
+        Element entry = doc.createElement("entry");
         
+        entry.setAttribute("row", row+"");
+        entry.setAttribute("col", col+"");
+        entry.setAttribute("value", sudoku.getValue(row, col)+"");
+        
+        return entry;
+    }
+
+    private static Document createDocument(Sudoku sudoku) throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("sudoku");
+
+        createElements(sudoku, doc, rootElement);
+        
+        return doc;
+    }
+
+    private static void storeDocument(Document doc, File file) throws TransformerConfigurationException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer t = tf.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(file);
+        t.transform(source, result);
     }
 }
